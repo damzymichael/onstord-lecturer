1fa1099b7c85a4c585696b8c2254ba75d3edf3d9
damzymichael <105460672+damzymichael@users.noreply.github.com>
2025-04-01 16:22:18 +0100

    Save changes

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -6,7 +6,10 @@ import {RequestBody} from '@/types/onstord';
 export const POST = async (request: NextRequest) => {
   try {
     const requestUser: RequestBody = await request.json();
-    const hashedPassword = await bcrypt.hash(requestUser.password as string, 10);
+    const hashedPassword = await bcrypt.hash(
+      requestUser.password as string,
+      10
+    );
     await prisma.lecturer.create({
       data: {...requestUser, password: hashedPassword}
     });
@@ -26,7 +29,22 @@ export const PATCH = async (request: NextRequest) => {
       data: {name, phoneNumber, institutions}
     });
     return new Response(JSON.stringify('Profile update successful'), {
-      status: 202
+      status: 200
+    });
+  } catch (error: any) {
+    return new Response(JSON.stringify('Could not update data'), {status: 500});
+  }
+};
+
+export const PUT = async (request: NextRequest) => {
+  try {
+    const password = await bcrypt.hash('oluwafred', 10);
+    await prisma.lecturer.update({
+      where: {id: '6569fb9d37b2bdbb5731facd'},
+      data: {password}
+    });
+    return new Response(JSON.stringify('Password update successful'), {
+      status: 200
     });
   } catch (error: any) {
     return new Response(JSON.stringify('Could not update data'), {status: 500});
